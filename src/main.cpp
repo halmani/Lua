99a907e2f0864935c12f6a7f4e80a73ef4d8dd49
halmani <halmani@hotmail.com>
2017-02-17 17:21:33 +0900

    リスト削除

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,6 @@
 int main()
 {
 	String string("Hello my string class!!");
-//	String string('c');
-//	String string(NULL);
-//	String string("my string \0class");
-
-//	const char* str = "Hello World!!\n";
-//	printf("new It\'s %s%s(%d)", str, string(), string.Size());
 	printf("%s\n", string());
     return 0;
 }
