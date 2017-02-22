66f8265c956cb40e25dcae98f4ef321db1ce7aa4
halmani <halmani@hotmail.com>
2017-02-22 21:17:50 +0900

    luaの関連付けが出来た様なのでコミット

---
+++ cmake/FindLua.cmake
@@ -0,0 +1,9 @@
+# - Find lua
+# this module looks for Lua
+#
+#  LUA_EXECUTABLE - the full path to lua
+#  LUA_LIBRARIES - the lua shared library
+#  LUA_INCLUDE_DIR - directory for lua includes
+#  LUA_PACKAGE_PATH - where Lua searches for Lua packages
+#  LUA_PACKAGE_CPATH - where Lua searches for library packages
+#  LUA_FOUND      - If false, don't attempt to use lua.
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include "../lib/lua-5.3.4/install/include/lua.hpp"
+#include "lua.hpp"
 #include "String.h"
 
 int main()
 {
-	String string("Hello my string class!!");
+	String string("Hello my String class!!");
 	printf("%s\n", string());
     return 0;
 }
